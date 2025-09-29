9645690b69998c4d22bb2aca4edd9fac3442d536
Caasi <isaacstocks2010@gmail.com>
2025-09-29 21:06:57 +0100

    Force test

--- main.py
+++ main.py
@@ -5,12 +5,9 @@ kit = ServoKit(channels=16)
 
 kit.servo[0].set_pulse_width_range(400, 2500)
 
-kit.servo[0].angle = 45
-time.sleep(1)
 kit.servo[0].angle = 90
 time.sleep(1)
-kit.servo[0].angle = 135
-time.sleep(1)
-kit.servo[0].angle = 180
-time.sleep(1)
 kit.servo[0].angle = 0
+time.sleep(5)
+kit.servo[0].angle = 90
+time.sleep(1)
