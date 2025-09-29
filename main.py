77b986e3168a900575e1f08fcb309042161466d9
Caasi <isaacstocks2010@gmail.com>
2025-09-29 20:48:00 +0100

    Added time sleep

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ kit = ServoKit(channels=16)
 kit.servo[0].set_pulse_width_range(400, 2500)
 
 kit.servo[0].angle = 0
+time.sleep(1)
 kit.servo[0].angle = 180
+time.sleep(1)
 kit.servo[0].angle = 90
+time.sleep(1)
 kit.servo[0].angle = 135
+time.sleep(1)
 kit.servo[0].angle = 45
+time.sleep(1)
 kit.servo[0].angle = 0
