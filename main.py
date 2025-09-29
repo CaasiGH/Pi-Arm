e1a58beda7169948cd819d170f125e560a81fdcb
Caasi <isaacstocks2010@gmail.com>
2025-09-29 20:50:12 +0100

    Changed Angles

--- main.py
+++ main.py
@@ -5,14 +5,12 @@ kit = ServoKit(channels=16)
 
 kit.servo[0].set_pulse_width_range(400, 2500)
 
-kit.servo[0].angle = 0
-time.sleep(1)
-kit.servo[0].angle = 180
+kit.servo[0].angle = 45
 time.sleep(1)
 kit.servo[0].angle = 90
 time.sleep(1)
 kit.servo[0].angle = 135
 time.sleep(1)
-kit.servo[0].angle = 45
+kit.servo[0].angle = 180
 time.sleep(1)
 kit.servo[0].angle = 0
