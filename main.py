70168451973b49ebf5c1f0d11f1c61c1af2e4125
Caasi <isaacstocks2010@gmail.com>
2025-09-29 20:25:38 +0100

    Servo Testing

---
+++ main.py
@@ -0,0 +1,10 @@
+from adafruit_servokit import ServoKit
+kit = ServoKit(channels=16)
+
+kit.servo[0].set_pulse_width_range(400, 2500)
+
+kit.servo[0].angle = 0
+kit.servo[0].angle = 180
+kit.servo[0].angle = 90
+kit.servo[0].angle = 135
+kit.servo[0].angle = 45
