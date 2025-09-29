85e14f63d89ef6ced97dca541ee8d8ad7154e872
Caasi <isaacstocks2010@gmail.com>
2025-09-29 21:11:14 +0100

    Multiple Servo Test

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 kit.servo[0].set_pulse_width_range(400, 2500)
+kit.servo[1].set_pulse_width_range(400, 2500)
 
-kit.servo[0].angle = 90
-time.sleep(1)
 kit.servo[0].angle = 0
+time.sleep(1)
+kit.servo[0].angle = 180
 time.sleep(5)
-kit.servo[0].angle = 90
+kit.servo[0].angle = 0
+time.sleep(1)
+
+kit.servo[1].angle = 0
+time.sleep(1)
+kit.servo[1].angle = 180
+time.sleep(5)
+kit.servo[1].angle = 0
 time.sleep(1)
