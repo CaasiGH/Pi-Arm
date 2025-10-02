48ff9c8bfee2979a4b71a171d87dd5468e13642a
Caasi <isaacstocks2010@gmail.com>
2025-10-02 22:44:41 +0100

    Removed second servo

--- main.py
+++ main.py
@@ -4,14 +4,10 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 kit.servo[0].set_pulse_width_range(400, 2500)
-kit.servo[1].set_pulse_width_range(400, 2500)
 
 kit.servo[0].angle = 0
-kit.servo[1].angle = 0
 time.sleep(2)
 kit.servo[0].angle = 180
-kit.servo[1].angle = 180
 time.sleep(2)
 kit.servo[0].angle = 0
-kit.servo[1].angle = 0
 time.sleep(2)
